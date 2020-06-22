dca804faa86b8288dcca433a46909af0f93bf5c7
dogyeong <shroad1802@naver.com>
2020-06-22 23:53:56 +0900

    Liking and Unliking our Posts

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -46,10 +46,30 @@ exports.deletePost = () => {}
 
 exports.getPostById = () => {}
 
-exports.getPostsByUser = () => {}
+exports.getPostsByUser = async (req, res) => {
+  const posts = await Post.find({ postedBy: req.profile._id }).sort({ createdAt: 'desc' })
+  res.json(posts)
+}
 
-exports.getPostFeed = () => {}
+exports.getPostFeed = async (req, res) => {
+  const { following, _id } = req.profile
+  following.push(_id)
+  const posts = await Post.find({ postedBy: { $in: following } }).sort({ createdAt: 'desc' })
+  res.json(posts)
+}
 
-exports.toggleLike = () => {}
+exports.toggleLike = async (req, res) => {
+  const { postId } = req.body
+  const post = await Post.findOne({ _id: postId })
+  const likeIds = post.likes.map((id) => id.toString())
+  const authUserId = req.user._id.toString()
+  if (likeIds.includes(authUserId)) {
+    await post.likes.pull(authUserId)
+  } else {
+    await post.likes.push(authUserId)
+  }
+  await post.save()
+  res.json(post)
+}
 
 exports.toggleComment = () => {}
